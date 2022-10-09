9300e0ce4fb34e92983be4dc339dac5a177fd137
Paul Ibeabuchi <ibeabuchi@worklio.cz>
2022-10-09 22:59:49 +0100

    created Month component and style

--- src/components/Months.js
+++ src/components/Months.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import './Month.css';
 
 function Months() {
+	const date = new Date();
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+	];
+
+	const allMonths = [];
+	for (let i = 0; i < months.length; i++) {
+		if (date.getMonth() === i) allMonths.push(<p className="current-month month" key={i}> { months[i] } </p>);
+		else allMonths.push(<p className="month" key={i}> { months[i] } </p>);
+	}
+
   return (
     <div className="months">
-        {/* List all months */}
-        {/* <Month /> */}
+			<h2>Select Month</h2>
+			<section className="allMonths">
+				{ allMonths }
+			</section>
     </div>
-  )
+  );
 }
 
 export default Months;
