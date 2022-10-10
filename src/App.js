4c50cf456d32047e667bc4243f4761a40c8e076a
Paul Ibeabuchi <ibeabuchi@worklio.cz>
2022-10-10 01:43:14 +0100

    added states and logic for calendee app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Calendar from './components/Calendar';
 import Months from './components/Months';
 import Years from './components/Years';
 
 function App() {
-  console.log(Months);
+  // define states
+  const [currentView, setCurrentView] = useState("calendar");
+  const [selectedYear, setSelectedYear] = useState("");
+  const [selectedMonth, setSelectedMonth] = useState("");
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  // initialize dates
+  useEffect(() => {
+    const date = new Date();
+    setSelectedMonth(date.getMonth() + 1);
+    setSelectedYear(date.getFullYear());
+  }, []);
+
+  // compute date based on selected month and year
+  useEffect(() => {
+    setSelectedDate( new Date(`${selectedYear} ${selectedMonth} 01`) );
+    console.log(selectedDate);
+    // eslint-disable-next-line 
+  }, [selectedYear, selectedMonth]);
+
   return (
     <div className="App">
-      <Calendar />
-      {/* <Months /> */}
-      {/* <Years /> */}
+      { 
+        currentView === "calendar" && 
+        <Calendar 
+          setCurrentView={setCurrentView} 
+          selectedMonth={selectedMonth}
+          selectedYear={selectedYear}
+          selectedDate={selectedDate}
+        />
+      }
+
+      { 
+        currentView === "months" && 
+        <Months setCurrentView={setCurrentView}  setSelectedMonth={setSelectedMonth}/>
+      }
+
+      { 
+        currentView === "years" && 
+        <Years setCurrentView={setCurrentView} setSelectedYear={setSelectedYear} />
+      }
     </div>
   );
 }
