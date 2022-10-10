88334f213f93308ad62ec9592a47c1ec4c05dd29
Paul Ibeabuchi <ibeabuchi@worklio.cz>
2022-10-10 01:45:16 +0100

    added utils folder

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,1 +1,24 @@
-// implment months and dates function here to return dates
+//  months and dates assets
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const specialMonths = {
+    "February": 28,
+    "April": 30,
+    "June": 30,
+    "November": 30
+}
+
+module.exports = {months, specialMonths}
