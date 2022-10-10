986c5c1e236c39b575bbdba24507fcf17747b4f7
Paul Ibeabuchi <ibeabuchi@worklio.cz>
2022-10-10 01:44:55 +0100

    added years component

--- src/components/Years.js
+++ src/components/Years.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useRef } from 'react';
+import './Years.css';
 
-function Years() {
+function Years({setCurrentView, setSelectedYear}) {
+  const currentYear = useRef();
+  const date = new Date();
+  const year = date.getFullYear();
+  const allYears = [];
+
+  // get next 100 and previous 100 years
+  for (let i = year + 100; i > year - 100; i--){
+		if (year === i) {
+      allYears.push(
+        <p onClick={() => handleClick(i)} ref={currentYear} className="current-year year" key={i}> { i } </p>
+      );
+    } else {
+      allYears.push(
+        <p onClick={() => handleClick(i)} className="year" key={i}> { i } </p>
+      );
+    }
+  }
+
+  // set year and go back to calendar view
+  function handleClick(value){
+    setCurrentView("calendar");
+    setSelectedYear(value);
+  }
+  
+  // scroll current year into view
+  useEffect(() => {
+    currentYear.current.scrollIntoView({block: "center"});
+  }, [])
+
   return (
-    <div>Years</div>
+    <div className="years">
+			<h2>Select Year</h2>
+			<section className="allYears">
+				{ allYears }
+			</section>
+    </div>
   )
 }
 
